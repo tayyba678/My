b053ab363091175f1b8f1d396f144519c5206409
Tayyba Aftab <144408026+tayyba678@users.noreply.github.com>
2023-10-18 10:40:28 +0500

    Add files via upload

--- tas5.cpp
+++ tas5.cpp
@@ -11,7 +11,8 @@ main()
 }
 int digitSum(int number)
 {
-    int n1, n2, a, sum = 0;
+    int n1, n2, n3, n4, n5, n6, a, sum = 0;
+    int nn = number;
     if (number == 0)
     {
         return 0;
@@ -21,20 +22,58 @@ int digitSum(int number)
 
     {
         number = number / 10;
+        ++nu;
     }
-    ++nu;
-
-    if (nu == 1)
+    int b = nu;
+    if (b == 1)
     {
 
-        a = number;
+        a = nn;
     }
 
-    if (number != 0 && nu == 2)
+    if (b == 2)
     {
-        n1 = number / 10;
-        n2 = number % 10;
+        n1 = nn % 10;
+        n2 = (nn / 10) % 10;
         sum = n1 + n2;
+        a = sum;
+    }
+    if (b == 3)
+    {
+
+        n1 = nn % 10;
+        n2 = (nn / 10) % 10;
+        n3 = (nn / 100) % 10;
+        sum = n1 + n2 + n3;
+        a = sum;
+    }
+    if (b == 4)
+    {
+        n1 = nn % 10;
+        n2 = (nn / 10) % 10;
+        n3 = (nn / 100) % 10;
+        n4 = (nn / 1000) % 10;
+        sum = n1 + n2 + n3 + n4;
+        a = sum;
+    }
+    if (b == 5)
+    {
+        n1 = nn % 10;
+        n2 = (nn / 10) % 10;
+        n3 = (nn / 100) % 10;
+        n4 = (nn / 1000) % 10;
+        n5 = (nn / 10000) % 10;
+        sum = n1 + n2 + n3 + n4 + n5;
+        a = sum;
+    }
+    if (b == 6)
+    {
+        n1 = nn % 10;
+        n2 = (nn / 10) % 10;
+        n3 = (nn / 100) % 10;
+        n4 = (nn / 1000) % 10;
+        n5 = (nn / 10000) % 10;
+        sum = n1 + n2 + n3 + n4 + n5 + n6;
         a = sum;
     }
     return a;
